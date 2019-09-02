f0558433081df599626026f23af0a2a8a74b8490
unknown <raziman.terra@gmail.com>
2019-09-02 22:55:03 +0800

    new

--- src/rain.js
+++ src/rain.js
@@ -3,9 +3,10 @@ module.exports = {
 
         let channel = bot.guilds.get('612515458767388698').channels.get('615734083678371841');
         if(msg.channel.id === "478074557232578561"){
-            channel.send(`${msg.content} 
+            channel.send(`=====================================
+${msg.content} 
             
-from ${msg.author.username}`);
+from ${msg.author.user}`);
         }
     }
 }
