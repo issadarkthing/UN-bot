aaa95ae99d68b8b3f8193d18834b77a7152a936e
unknown <raziman.terra@gmail.com>
2019-09-03 00:15:56 +0800

    new

--- src/rain.js
+++ src/rain.js
@@ -2,11 +2,13 @@ module.exports = {
     execute(msg, bot){
 
         let channel = bot.guilds.get('612515458767388698').channels.get('615734083678371841');
+        let name = msg.guild.members.find(m => m.id === msg.author.id).displayName;
         if(msg.channel.id === "478074557232578561"){
-            channel.send(`=====================================
+            channel.send(`
 ${msg.content} 
             
-from ${msg.author.user}`);
+from \`${name}\`
+==========================================`);
         }
     }
 }
