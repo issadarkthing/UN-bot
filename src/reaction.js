13f1de99c8d4e8e6c78e127409707d799987199f
unknown <raziman.terra@gmail.com>
2019-09-12 12:27:15 +0800

    new

--- src/reaction.js
+++ src/reaction.js
@@ -1,10 +1,10 @@
 module.exports = {
   execute(msg){
     let text = msg.content.toLowerCase();
-    if(text.includes('DOS')){
+    if(text.includes('dos')){
       msg.react('621197617832919070');
     }
-    if(text.includes('Department of Suggestions')){
+    if(text.includes('department of suggestions')){
       msg.react('621197617832919070');
     }
     if(text.includes('test')){
